e8e379050f67e2fbdd6dd5d9a1187315d0f0aab2
KorallenLang <aneekah.younus986@gmail.com>
2022-12-14 23:56:37 -0500

    First commit

--- views/script.js
+++ views/script.js
@@ -1,8 +1,25 @@
-let tds = document.querySelectorAll('#table1 td');
-let td = Array.from(tds);
-let button = document.getElementById("send1");
+let button2 = document.getElementById("send2");
+function onClick() {
+    let tbody = document.getElementById("tbody1");
+    let numberOfWord = document.getElementById("options");
+    let userChoice = numberOfWord.options[numberOfWord.selectedIndex].text;
+    for(let i = 0; i < userChoice; i++) {
+        const newTableRow = document.createElement("tr");
+        const keyRow = document.createElement("td");
+        const valRow = document.createElement("td");
+        tbody.appendChild(newTableRow);
+        newTableRow.appendChild(keyRow);
+        newTableRow.appendChild(valRow);
+    }
+}
+
+button2.addEventListener('click', onClick);
+
+let button1 = document.getElementById("send1");
 
 async function onClick() {
+    let tds = document.querySelectorAll('#table1 td');
+    let td = Array.from(tds);
     let urlInput = document.getElementById("user_input").value;
     document.getElementById('user_input').value = '';
     const response = await axios.post("/scrape", {URL: urlInput});
@@ -16,4 +33,4 @@ async function onClick() {
     }
 }
 
-button.addEventListener('click', onClick);
+button1.addEventListener('click', onClick);
